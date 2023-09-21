b23307bcd9ffbbfd3a3a4cff2e20db633d79c4ae
faceyourfears <roisensthorn@gmail.com>
2023-09-21 14:03:07 +0800

    Initial Commit

---
+++ Lab2/function.cpp
@@ -0,0 +1,39 @@
+#include "lab2.hh"
+#include <iostream>
+
+void read(int *A, int n){
+    for(int i = 0; i < n; i++){
+        std::cin >> A[i];
+    }
+}
+
+void print(int *A, int n){
+    for(int i = 0; i < n; i++){
+        std::cout << A[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int max(int *A, int n){
+
+}
+
+int gcd(int *A, int n){
+
+}
+
+int * concat(int *A, int n, int *B, int m){
+
+}
+
+int * subarray(int *A, int n, int start, int len){
+
+}
+
+int * add(int * A, int n, int p, int e){
+
+}
+
+int * del(int *A, int n, int p){
+
+}
