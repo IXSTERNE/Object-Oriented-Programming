fbff9d3239152d6baa62512d8cb0328303d0c5d3
hugemommiebonkers <adcsucks2000@gmail.com>
2023-11-08 22:31:07 +0800

    main.cpp/comment

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -9,18 +9,18 @@ int main()
         cin>>a; // 1/2
         cin>>b; // 2/5
 
-        EngBut c(0, 1); // 0
-        EngBut d(1, 1); // 1
+        // EngBut c(0, 1); // 0
+        // EngBut d(1, 1); // 1
 
-        cout<< a + b << endl; // 9/10
-        cout<< a * b << endl; // 1/10
-        cout<< a - b << endl; // 1/5
-        cout<< a / b << endl; // 5/4
-        cout<< a + 1 << endl; // 3/2
-        cout<< a - 2 << endl; // -3/2
-        cout<< a * 3 << endl; // 3/2
-        cout<< a / 4 << endl; // 1/8
-        cout<< c - d << endl; // -1/1
+        // cout<< a + b << endl; // 9/10
+        // cout<< a * b << endl; // 1/10
+        // cout<< a - b << endl; // 1/5
+        // cout<< a / b << endl; // 5/4
+        // cout<< a + 1 << endl; // 3/2
+        // cout<< a - 2 << endl; // -3/2
+        // cout<< a * 3 << endl; // 3/2
+        // cout<< a / 4 << endl; // 1/8
+        // cout<< c - d << endl; // -1/1
 
         return 0;
 }
