006b75ca39b2df67afbe7a9abd3341faf791db4b
hugemommiebonkers <adcsucks2000@gmail.com>
2023-11-15 15:52:05 +0800

    init

---
+++ Lab5/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        Fifo<int> fi(3);
+        Fifo<char> fc(3);
+
+        fi.write(3);
+        fi.write(5);
+        fi.write(7);
+        fc.write('a');
+        fc.write('b');
+        fc.write('c');
+        cout<< fi.read() << endl; // 3
+        cout<< fc.read() << endl; // a
+
+        return 0;
+}
