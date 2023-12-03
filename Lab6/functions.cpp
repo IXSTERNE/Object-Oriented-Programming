262639b2b81c1817c8c436520d1afe34af2d49bf
xjoules <rubyxjoules@gmail.com>
2023-12-03 13:56:34 +0800

    working

---
+++ Lab6/functions.cpp
@@ -0,0 +1,46 @@
+#include "lab6.hh"
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+Rectangle::Rectangle(double a, double b){
+    this->a = a;
+    this->b = b;
+}
+
+double Rectangle::area(){
+    return a * b;
+}
+
+double Rectangle::perimeter(){
+    return (a + b) * 2;
+}
+
+Triangle::Triangle(double a, double b, double c){
+    this->a = a;
+    this->b = b;
+    this->c = c;
+}
+
+double Triangle::area(){
+    semi_p = (a + b + c) / 2;
+    return sqrt(semi_p * (semi_p - a) * (semi_p - b) * (semi_p - c));
+}
+
+double Triangle::perimeter(){
+    return a + b + c;
+}
+
+Circle::Circle(double r){
+    this->r = r;
+}
+
+double Circle::area(){
+    return pi * pow(r, 2);
+}
+
+double Circle::perimeter(){
+    return 2 * pi * r;
+}
---
+++ Lab6/lab6.hh
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+
+class Shape{
+    public:
+        virtual double area() = 0;
+        virtual double perimeter() = 0;
+    protected:
+        double pi = M_PI;
+        // protected : variables cannot be accessed from 
+        // outside the class but possible to access it via inheritance
+        // private : variabkes cannot be accessed from out of the class
+};
+
+class Rectangle : public Shape{
+    public:
+        Rectangle(double, double);
+        double area();
+        double perimeter();
+    private:
+        double a;
+        double b;
+};
+
+class Triangle : public Shape{
+    public:
+        Triangle(double, double, double);
+        double area();
+        double perimeter();
+    private:
+        double a;
+        double b;
+        double c;
+        double semi_p;
+};
+
+class Circle : public Shape{
+    public:
+        Circle(double);
+        double area();
+        double perimeter();
+    private:
+        double r;
+};
