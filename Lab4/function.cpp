b6814ab06714d49e7a11c8a262c77a597ba6a2ee
xjoules <rubyxjoules@gmail.com>
2023-12-03 14:36:33 +0800

    gcd

--- Lab4/function.cpp
+++ Lab4/function.cpp
@@ -4,51 +4,79 @@
 using namespace std;
 
 EngBut::EngBut(int hurtver, int huvaari){
+
     number = hurtver;
     denominator = huvaari;
 }
 
 istream& operator>>(istream& in, EngBut &object){
+
     cin >> object.number >> object.denominator;
     return cin;
 }
 
 ostream& operator<<(ostream& out, const EngBut& object){
+
     cout << object.number << '/' << object.denominator;
     return cout;
 }
 
-// void EngBut::refactor(int& re_hurtver, int& re_huvaari){
-//     if(re_hurtver % re_huvaari == 0){
-//         re_hurtver = re_hurtver / re_huvaari;
-//         re_huvaari = re_huvaari / re_huvaari;
-//     }
-// }
+int EngBut::gcd(int a, int b){
+
+    int result = min(a, b);
+    while(result > 0){
+        if (a % result == 0 && b % result == 0){
+            break;
+        }
+        result--;
+    }
+    return result;
+}
+
+void EngBut::refactor(int& re_hurtver, int& re_huvaari){
+
+    simplifier = gcd(re_hurtver, re_huvaari);
+
+    re_hurtver = re_hurtver / simplifier;
+    re_huvaari = re_huvaari / simplifier;
+}
 
 EngBut EngBut::operator+(const EngBut& object){
+
     int hurtver = (number * object.denominator) + (denominator * object.number);
     int huvaari = denominator * object.denominator;
+
+    refactor(hurtver, huvaari);
     
     return EngBut(hurtver, huvaari);
 }
 
 EngBut EngBut::operator-(const EngBut &object){
+
     int hurtver = (number * object.denominator) - (denominator * object.number);
     int huvaari = denominator * object.denominator;
+
+    refactor(hurtver, huvaari);
     
     return EngBut(hurtver, huvaari);
 }
 
 EngBut EngBut::operator*(const EngBut &object){
+
     int hurtver = number * object.number;
     int huvaari = denominator * object.denominator;
+
+    refactor(hurtver, huvaari);
 
     return EngBut(hurtver, huvaari);
 }
 
 EngBut EngBut::operator/(const EngBut &object){
+
     int hurtver = number * object.denominator;
     int huvaari = denominator * object.number;
+
+    refactor(hurtver, huvaari);
 
     return EngBut(hurtver, huvaari);
 }
